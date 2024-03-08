ae09aa6fcf092abe7951f3f6335a15233254cf01
BioBootloader <thebiobootloader@gmail.com>
2024-03-07 23:24:18 -0800

    add SpiceClient

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
-from spice import call_llm
+from spice import SpiceClient
+
+# model = "gpt-4-0125-preview"
+model = "claude-3-opus-20240229"
 
 system_message = "You are a helpful assistant."
 messages = [
     {"role": "user", "content": "list 5 random words"},
 ]
 
-for t in call_llm(system_message, messages, stream=True):
+client = SpiceClient(model=model)
+
+for t in client.call_llm(system_message, messages, stream=True):
     print(t, end="")
 
 print("\n####################\n")
 
-response = call_llm(system_message, messages, stream=False)
+response = client.call_llm(system_message, messages, stream=False)
 print(response)
