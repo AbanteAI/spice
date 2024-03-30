d6d73323df66e86505d60080ad11961be39ad801
BioBootloader <thebiobootloader@gmail.com>
2024-03-30 14:46:32 -0700

    improve

--- spice/whisper.py
+++ spice/whisper.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
+
 import os
 from pathlib import Path
-from typing import TYPE_CHECKING
 
-from openai import AsyncOpenAI
-from azure.ai.textanalytics.aio import TextAnalyticsClient
+from openai import AsyncAzureOpenAI, AsyncOpenAI
 
-if TYPE_CHECKING:
-    from openai.api_resources.async_resources import AsyncAudio
+from spice.errors import SpiceError
+
 
 class SpiceWhisper:
     def __init__(self, provider: str = "openai"):
@@ -15,20 +14,26 @@ class SpiceWhisper:
             base_url = os.getenv("OPENAI_API_BASE")
             key = os.getenv("OPENAI_API_KEY")
             if key is None:
-                raise ValueError("OPENAI_API_KEY not set")
-            self.async_client = AsyncOpenAI(api_key=key, base_url=base_url)
+                if base_url:
+                    # Using a dummy key for LiteLLM proxy setup where OPENAI_API_BASE is set but OPENAI_API_KEY is not required.
+                    key = "dummy_key_base_url"
+                else:
+                    raise SpiceError("OPENAI_API_KEY not set")
+            self._client = AsyncOpenAI(api_key=key, base_url=base_url)
         elif provider == "azure":
             key = os.getenv("AZURE_OPENAI_KEY")
             endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-            if key is None or endpoint is None:
-                raise ValueError("AZURE_OPENAI_KEY or AZURE_OPENAI_ENDPOINT not set")
-            self.async_client = TextAnalyticsClient(endpoint=endpoint, credential=key)
+            if key is None:
+                raise SpiceError("AZURE_OPENAI_KEY not set")
+            if endpoint is None:
+                raise SpiceError("AZURE_OPENAI_ENDPOINT not set")
+            self._client = AsyncAzureOpenAI(api_key=key, api_version="2023-12-01-preview", azure_endpoint=endpoint)
         else:
-            raise ValueError(f"Unknown provider: {provider}")
+            raise SpiceError(f"Unknown whisper provider: {provider}")
 
-    async def call_whisper_api(self, audio_path: Path) -> str:
+    async def get_whisper_transcription(self, audio_path: Path) -> str:
         async with open(audio_path, "rb") as audio_file:
-            transcript = await self.async_client.audio.transcriptions.create(
+            transcript = await self._client.audio.transcriptions.create(
                 model="whisper-1",
                 file=audio_file,
             )
