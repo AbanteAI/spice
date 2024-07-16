09514101035fb4380200c25e41aa5499df57d443
granawkins <granthawkins88@gmail.com>
2024-07-16 09:54:44 +0700

    tweak comment

--- spice/retry_strategy/__init__.py
+++ spice/retry_strategy/__init__.py
@@ -6,9 +6,9 @@ from spice.spice import SpiceCallArgs
 
 T = TypeVar("T")
 
-# An object that defines how get_response should validate/convert a response. It must implement the decide and
-# get_retry_name methods. The decide method takes in the previous call_args, which attempt number this is, and the model
-# output. The method returns a tuple of Behavior, SpiceCallArgs, and the result the result and the name of the run. If
+# An object that defines how get_response should validate/convert a response. It must implement the decide method.
+# It takes in the previous call_args, which attempt number this is, and the model output.
+# The method returns a tuple of Behavior, SpiceCallArgs, the result and the name of the run. If
 # the Behavior is RETURN, then the result will be returned as the result of the SpiceResponse object. If the Behavior
 # is RETRY, then the llm will be called again with the new spice_args. It's up to the Behavior to eventually return
 # RETURN or throw an exception.
