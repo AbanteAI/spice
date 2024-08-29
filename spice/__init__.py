16e6cace07222ab8de63ce062fabbad19059036b
scotttestbot <176420715+scotttestbot@users.noreply.github.com>
2024-08-28 21:39:29 -0700

    thing

--- spice/__init__.py
+++ spice/__init__.py
@@ -1,2 +1,3 @@
 from .spice import Spice, SpiceResponse, StreamingSpiceResponse, EmbeddingResponse, TranscriptionResponse  # noqa
 from .utils import print_stream  # noqa
+from .spice_message import SpiceMessages, SpiceMessage  # noqa
